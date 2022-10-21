03d9d3ad9dc2a9fb5292b70dc73174501e79b438
Galina <galina.tkachenko@ses.com>
2022-10-21 16:48:36 +0200

    My Django Girls app, first commit

---
+++ main.py
@@ -0,0 +1,10 @@
+# This is a sample Python script.
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+from django.utils import timezone
+
+cia=timezone.now()
+print(cia)
+# See PyCharm help at https://www.jeotbrains.com/help/pycharm/
